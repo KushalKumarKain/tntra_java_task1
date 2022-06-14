e1a7213ce8ad20a49feaed2172089e410363a820
Kushal Kumar Kain <85063289+KushalKumarKain@users.noreply.github.com>
2022-06-14 16:59:19 +0530

    Editied js code to get proper validations for fields that require only alphabetical inputs.

--- jquery/JAVAtask3.js
+++ jquery/JAVAtask3.js
@@ -4,25 +4,31 @@ $(document).ready(function(){
     rules:{
       f_name :{
         required: true,
-        lettersonly : true
+        lettersonly : true,
+        min : 3
       },
       l_name : {
         required : true,
-        lettersonly : true
+        lettersonly : true,
+        min : 3
       },
       addressline1 : {
-        required : true
+        required : true,
+        min : 10
       },
       addressline2 : {
-        required : true
+        required : true,
+        min : 5
       },
       city : {
         required : true,
-        lettersonly : true
+        lettersonly : true,
+        min :3
       },
       state : {
         required : true,
-        lettersonly : true
+        lettersonly : true,
+        min : 3
       },
       postal : {
         required : true,
@@ -35,16 +41,18 @@ $(document).ready(function(){
         min : 10
       },
       email : {
-        required : true
+        required : true,
       },
       dropdown : {
         required : true
       },
       feedback : {
-        required : true
+        required : true,
+        min : 15
       },
       suggestions : {
-        required : true
+        required : true,
+        min : 15
       },
       gender : {
         required : true
@@ -53,25 +61,31 @@ $(document).ready(function(){
     messages : {
       f_name : {
         required : "This is required",
-        lettersonly : "Enter only alphabets"
+        lettersonly : "Enter only alphabets",
+        min : "First name should be grater than 2 alphabets"
       },
       l_name : {
         required : "This is required",
-        lettersonly : "Enter only alphabets"
+        lettersonly : "Enter only alphabets",
+        min : "Last name should be grater than 2 alphabets "
       },
       addressline1 : {
-        required : "This is required"
+        required : "This is required",
+        min : "Address can not be less than 10 characters"
       },
       addressline2 : {
         required : "This is required",
+        min : "Address can not be less than 10 characters"
       },
       city : {
         required : "This is required",
-        lettersonly : "Enter only alphabets"
+        lettersonly : "Enter only alphabets",
+        min: "City name cannot be less than 3 characters"
       },
       state : {
         required : "This is required",
-        lettersonly : "Enter only alphabets"
+        lettersonly : "Enter only alphabets",
+        min : "State name can not be less than 3 characters"
       },
       postal : {
         required : "This is required",
@@ -83,11 +97,6 @@ $(document).ready(function(){
         Number : "Enter only number",
         min : "Enter only 10 digits"
       },
-      postal : {
-        required : "This is required",
-        Number : "Enter only number",
-        min : "Enter only 6 digits"
-      },
       email : {
         required : "This is required"
       },
@@ -95,10 +104,12 @@ $(document).ready(function(){
         required : "This is required"
       },
       feedback : {
-        required : "This is required"
+        required : "This is required",
+        min : "Feedback cannot be less than 15 characters"
       },
       suggestions : {
-        required : "This is required"
+        required : "This is required",
+        min : "Suggestions cannot be less than 10 characters"
       },
       gender : {
         required : "This is required"
