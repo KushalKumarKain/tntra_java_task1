28c1b4518a4d4b138ae1f188d229d123778a5a55
Kushal Kumar Kain <85063289+KushalKumarKain@users.noreply.github.com>
2022-06-14 11:23:09 +0530

    incomplete jquery for task 2 and implementing jquery validator on form

--- jquery/JAVAtask2.js
+++ jquery/JAVAtask2.js
@@ -1,7 +1,62 @@
-$(document).ready(function()
-{
-  $("#f_name").hide();
+
+$(document).ready(function(){
+
+  // function for first name
+  $("#first_name").hide();
+  let firstname_err = true;
+  $("#f_name").keyup(function(){
+    validatefirstname();
+  });
+
+  function validatefirstname(event){
+    let firstnamevalue = $("#f_name").val();
+    if (firstnamevalue.length == "" ){
+      $("#first_name").show();
+      firstname_err = false;
+      return false;
+    }
+    else ("#first_name").hide();
+  }
+
+  // function for last name
+  $("#last_name").hide();
+  let lastname_err = true;
+  $("#f_name").keyup(function(){
+    validatelastname();
+  });
+
+  debugger
+  function validatelastname(event){
+    
+    let lastnamevalue = $("#l_name").val();
+    if (lastnamevalue.length == "" ){
+      $("#last_name").show();
+      lastname_err = false;
+      return false;
+    }
+    else ("#last_name").hide();
+  }
+
+
+  $("#submitbtn").click(function(event){
+
+    
+    
+    validatefirstname();
+    validatelastname();
+    event.preventDefault();
+    if (
+      firstname_err == true,
+      lastname_err == true  
+    )
+    return true;
+    else {
+      return false;
+    }
   
+  
+  });
+
 
 
 
